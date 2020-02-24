ab2402b8ebccd02bff2a2fade9060944dab7a353
Jodi Francis <jodiannfrancis3@gmail.com>
2020-02-23 22:40:14 -0800

    DiceSet

--- DiceSet.java
+++ DiceSet.java
@@ -1,9 +1,6 @@
-import java.util.Arrays;
 import java.util.List;
-import java.util.Collections;
-
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class DiceSet {
 
@@ -11,7 +8,7 @@ public class DiceSet {
 
     public DiceSet(int sidesOnEachDie, int numberOfDice) {
       if (numberOfDice < 2 ){
-        throw new IllegalArgumentException("At least two Dice are required");
+        throw new IllegalArgumentException("At least two dice required");
       }
       if (sidesOnEachDie < 4){
         throw new IllegalArgumentException("Dice must have at least four sides");
@@ -22,78 +19,65 @@ public class DiceSet {
       }
     }
 
-    /**
-     * Creates a DiceSet where each die has the given number of sides, with the
-     * given values.
-     */
     public DiceSet(int sidesOnEachDie, int... values) {
-        // TODO
-    }
+      if (values.length < 2) {
+       throw new IllegalArgumentException("At least two dice required");
+     }
+     if (sidesOnEachDie < 4) {
+       throw new IllegalArgumentException("Dice must have at least four sides");
+     }
+        this.dice= new Die[values.length];
+        for (var i=0; i < values.length; i++) {
+            dice[i]=new Die(sidesOnEachDie, values[i]);
+        }
+      }
 
-    /**
-     * Returns the descriptor of the dice set; for example "5d20" for a set with
-     * five dice of 20 sides each; or "2d6" for a set of two six-sided dice.
-     */
     public String descriptor() {
-        return "";
-
+        return this.dice.length + "d" + this.dice[0].getSides() ;
     }
 
-    /**
-     * Returns the sum of the values of each die in the set.
-     */
     public int sum() {
-        return 0;
+        int sum = 0;
+        for(var i = 0; i < this.dice.length; i++){
+            sum += this.dice[i].getValue();
+        }
+        return sum;
 
     }
 
-    /**
-     * Rolls all the dice in the set.
-     */
     public void rollAll() {
-        // TODO
-    }
+      for(var i = 0; i < this.dice.length; i++){
+           this.dice[i].roll();
+       }
+     }
 
-    /**
-     * Rolls the ith die, updating its value.
-     */
     public void rollIndividual(int i) {
-        // TODO
+        this.dice[i].roll();
     }
 
-    /**
-     * Returns the value of the ith die.
-     */
     public int getIndividual(int i) {
-        return 0;
-
+        return this.dice[i].getValue();
     }
 
-    /**
-     * Returns the values of each of the dice in a list.
-     */
     public List<Integer> values() {
-        return List.of();
+      var result = new ArrayList<Integer>();
+         for (var i = 0; i < dice.length; i++){
+           result.add(dice[i].getValue());
+         }
+         return result;
 
     }
 
-    /**
-     * Returns whether this dice set has the same distribution of values as
-     * an other dice set. The two dice sets must have the same number of dice
-     * and the same number of sides per dice, and there must be the same
-     * number of each value in each set.
-     */
     public boolean isIdenticalTo(DiceSet diceSet) {
-      return false;
+      return true;
 
     }
 
-    /**
-     * Returns a string representation in which each of the die strings are
-     * joined without a separator, for example: "[2][5][2][3]".
-     */
     @Override public String toString() {
-      return "";
-
-    }
+      var result = "";
+         for (var i = 0; i < dice.length; i++){
+             result += "[" + dice[i].getValue() + "]";
+         }
+           return result;
+         }
 }
--- HighRollerGame.java
+++ HighRollerGame.java
@@ -11,9 +11,11 @@ public class HighRollerGame {
                 var command = console.readLine("Enter a command (h for help): ").trim();
                 if (command.matches("h(elp)?")) {
                     showHelp();
+
                 } else if (command.matches("q(uit)?")) {
                     System.out.println("I'm glad you played today. You look great!");
                     break;
+
                 } else if (command.matches("use\\s+\\d\\d?\\s+\\d\\d?")) {
                     var tokens = command.split("\\s+");
                     var sides = Integer.parseInt(tokens[2].trim());
@@ -22,10 +24,14 @@ public class HighRollerGame {
                     highest = Math.max(highest, diceSet.sum());
                     System.out.println("You are now using a " + diceSet.descriptor());
                     System.out.println(diceSet);
+
                 } else if (command.matches("roll\\s+all")) {
                     if (diceSet == null) {
                         throw new IllegalStateException("You don't have any dice yet");
                     }
+
+                    diceSet.rollAll();
+                    =Math .mac
                     // TODO: Roll all, sum them up, and update the highest score so far
                     // if necessary, and then print out the dice set
                 } else if (command.matches("roll\\s+\\d+")) {
@@ -35,14 +41,19 @@ public class HighRollerGame {
                     diceSet.rollIndividual(Integer.parseInt(command.substring(4).trim()));
                     highest = Math.max(highest, diceSet.sum());
                     System.out.println(diceSet);
+
                 } else if (command.matches("high(est)?")) {
                     if (highest == 0) {
+
                         // TODO: Print that there is no highest score yet
+
                     } else {
                         // TODO: Print the highest score so far
+
                     }
                 } else {
                     System.out.println("I don't understand");
+
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
